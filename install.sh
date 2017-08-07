5ebb7fd3ad1ebe1faba5bfd296d064da45a98853
Christian Zufferey <christian@zufferey.com>
2017-08-07 16:58:22 +0200

    - premiers tests pas trop concluants. Il faut encore creuser pour savoir où cela coince ;-(

--- install.sh
+++ install.sh
@@ -39,8 +39,13 @@
 THEIP=$(/sbin/ifconfig ens18 | /bin/grep "inet ad" | /usr/bin/cut -f2 -d: | /usr/bin/awk '{print $1}')
 
 #echo "network.host: "$THEIP | sudo tee -a /etc/elasticsearch/elasticsearch.yml
-echo "network.bind_host: "$THEIP | sudo tee -a /etc/elasticsearch/elasticsearch.yml
-sudo service elasticsearch restart
+#echo "network.bind_host: "$THEIP | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+#sudo service elasticsearch restart
+
+
+echo "server.host: "$THEIP | sudo tee -a /opt/kibana/config/kibana.yml
+echo "elasticsearch.url: http://"$THEIP":9200" | sudo tee -a /opt/kibana/config/kibana.yml
+sudo service kibana restart
 
 
 
--- start.sh
+++ start.sh
@@ -2,6 +2,9 @@
 #petit script à lancer pour ue cela tourne !
 #zf170807.1613
 
+curl -XPUT http://sdf-elk-dev:9200/zuzu20170807.1631
+/opt/logstash/bin/logstash -f test.conf
+
 
 
 
