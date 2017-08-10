79ee35554442aa70a22a15108b73f3200511fbf2
Christian Zufferey <christian@zufferey.com>
2017-08-10 15:18:43 +0200

    - corrigé le bug du & dans le script restart.sh, il ne fonctionnait pas avec ;-(

--- restart_elk.sh
+++ restart_elk.sh
@@ -4,6 +4,6 @@
 
 #source: 
 
-sudo systemctl restart elasticsearch.service &
-sudo systemctl restart kibana.service &
+sudo systemctl restart elasticsearch.service
+sudo systemctl restart kibana.service
 
