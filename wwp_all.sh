6b4bfb1820e69350f1dd10de7a854bd8215a651c
Christian Zufferey <christian@zufferey.com>
2017-08-15 13:56:54 +0200

    test de merge

--- wwp_all.sh
+++ wwp_all.sh
@@ -2,6 +2,6 @@
 #petit script à lancer pour que cela tourne !
 #zf170808.1735
 
-export LS_HEAP_SIZE="100m"
+export LS_HEAP_SIZE="800m"
 curl -XDELETE http://10.92.104.198:9200/*
 /opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_all.conf
