35e4ea8d2cf7d3c21653cf5a560e94b1e2c9e2af
Christian Zufferey <christian@zufferey.com>
2017-08-15 08:08:32 +0200

    - ajouté export LS_HEAP_SIZE="80m" au script sh pour limiter la mémoire

--- dns_all_grok.sh
+++ dns_all_grok.sh
@@ -2,6 +2,7 @@
 #petit script à lancer pour que cela tourne !
 #zf170808.1735
 
+export LS_HEAP_SIZE="80m"
 
 curl -XDELETE http://10.92.104.200:9200/*
 /opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/dns_all_grok.conf
--- wwp_1000.sh
+++ wwp_1000.sh
@@ -2,8 +2,8 @@
 #petit script à lancer pour que cela tourne !
 #zf170808.1735
 
-export LS-HEAT_SIZE="100m"
-curl -XDELETE http://10.92.104.200:9200/distrib.170810.1725/
+export LS_HEAP_SIZE="100m"
+curl -XDELETE http://10.92.104.200:9200/*
 /opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_1000.conf
 
 
