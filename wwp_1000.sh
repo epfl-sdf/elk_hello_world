6ba1b30a548ee50712425689020b6a4e69b0458e
Louis Munier <louis.munier@epfl.ch>
2017-08-15 15:41:42 +0200

    - reparations erreur merge

--- wwp_1000.sh
+++ wwp_1000.sh
@@ -3,17 +3,5 @@
 #zf170808.1735
 
 export LS_HEAP_SIZE="100m"
-<<<<<<< HEAD
 curl -XDELETE http://10.92.104.200:9200/*
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-curl -XDELETE http://10.92.104.200:9200/*
-=======
-curl -XDELETE http://10.92.104.198:9200/*
->>>>>>> f092cf347b3d9a392bb38b2b05490f919724568f
-=======
-curl -XDELETE http://10.92.104.200:9200/*
->>>>>>> eb94f37f52efb0b9075f057af44e57b5fefa4b98
->>>>>>> 4ad078d6a00cffe9ac4dd969ecd1a96d2563e97b
 /opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_1000.conf
