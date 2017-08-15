61a9571b6c1b0830e55b77bb2efed533cfc13171
Louis Munier <louis.munier@epfl.ch>
2017-08-15 14:25:04 +0200

    - reparation de mes erreurs de merges

--- wwp_all_grok.sh
+++ wwp_all_grok.sh
@@ -3,9 +3,5 @@
 #zf170808.1735
 
 export LS_HEAP_SIZE="100m"
-<<<<<<< HEAD
 curl -XDELETE http://10.92.104.200:9200/*
-=======
-curl -XDELETE http://10.92.104.198:9200/*
->>>>>>> f092cf347b3d9a392bb38b2b05490f919724568f
 /opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_1000.conf
