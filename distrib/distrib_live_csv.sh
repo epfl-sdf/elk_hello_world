fabad0173de1d335473c7552d4db5a8769a59eb5
YoussefSaied <youssef.saied@epfl.ch>
2017-08-15 21:11:39 +0200

    _ Distrib termine avec elasticsearch filtr.

--- distrib/distrib_live_csv.sh
+++ distrib/distrib_live_csv.sh
@@ -2,9 +2,9 @@
 #petit script à lancer pour que cela tourne !
 #zf170808.1735,zf170809.1632
 
-export LS_HEAP_SIZE="100m"
+export LS_HEAP_SIZE="80m"
 curl -XDELETE http://sdf-elk-dev:9200/distrib.170810.1725
-/opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/distrib_live_csv.conf
+/opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/distrib/distrib_live_csv.conf
 
 
 
