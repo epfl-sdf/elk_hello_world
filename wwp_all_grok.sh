032e4aa324a8a796d96d7094039ac00933211785
Louis Munier <louis.munier@epfl.ch>
2017-08-15 17:09:18 +0200

    - push save avant nouvelle branche

--- wwp_all_grok.sh
+++ wwp_all_grok.sh
@@ -3,5 +3,5 @@
 #zf170808.1735
 
 export LS_HEAP_SIZE="80m"
-curl -XDELETE http://10.92.104.200:9200/*
-/opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_all.conf
+curl -XDELETE http://10.92.104.200:9200/wwp_log_%{+YYYY.MM.dd}
+/opt/logstash/bin/logstash -f /home/ubuntu/elk_hello_world/wwp_all_grok.conf
