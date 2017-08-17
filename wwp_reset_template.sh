cd068723c84c0ab576b9065e21322f741b4458af
Louis Munier <louis.munier@epfl.ch>
2017-08-17 14:39:30 +0200

    - mettre de l'ordre dans les fichiers

--- wwp_reset_template.sh
+++ wwp_reset_template.sh
@@ -3,6 +3,6 @@
 #zf170808.1735
 
 export LS_HEAP_SIZE="80m"
-curl -XDELETE http://louis-1:9200/wwp_log_20170817?pretty
+curl -XDELETE http://louis-1:9200/_template/wwp_log_20170817?pretty
 curl -XPUT http://louis-1:9200/_template/wwp_log_20170817?pretty -d @wwp_all_grok_mapping.json
 
